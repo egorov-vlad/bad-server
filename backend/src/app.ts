8b1331b2c79d283ec2c785571276f702d6144f9b
Vlad Egorov <kerberttv@gmail.com>
2024-12-10 17:21:36 +0300

    fix max ratelimit

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -15,7 +15,7 @@ const app = express()
 
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, 
+    max: 40,  
     message: 'Too many requests from this IP, please try again after 15 minutes',
     standardHeaders: true,
 })
